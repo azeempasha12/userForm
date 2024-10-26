5d60e4c20612a603c4d08105a80912392eb9ad9c
Azeem pasha <147592984+azeempasha12@users.noreply.github.com>
2024-10-26 15:10:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static("public"));
